c77792c770a666c4fbe29673a02442f8389ff053
Evelyn Arevalo <evelynarevalow@gmail.com>
2022-08-02 12:12:06 -0700

    removed confirmed passworded on model

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const Sequelize = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt') 
@@ -59,21 +60,7 @@ module.exports = (sequelize) => {
         }
       }
     },
-     confirmedPassword: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      set(val) {
-        if ( val === this.password ) {
-          const hashedPassword = bcrypt.hashSync(val, 10);
-          this.setDataValue('confirmedPassword', hashedPassword);
-        }
-      },
-      validate: {
-        notNull: {
-          msg: 'Both passwords must match'
-        }
-      }
-    }
+    
     
   }, { sequelize });
 
@@ -87,3 +74,4 @@ Users.hasMany(models.Course, {foreignKey: {
 
   return Users;
 };
+
